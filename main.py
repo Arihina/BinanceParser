78c0335015e8621fb16f4c9ae5cfb60d005089d3
Alena Kozlova <alena.kozlova2004@mail.ru>
2024-02-19 15:26:52 +0300

    added finding coins in dict

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+from pprint import pprint
+
 import bs4
 import fake_useragent
 import requests
@@ -8,4 +10,17 @@ header = {"user-agent": fake_useragent.UserAgent().random}
 response = requests.get(ref, headers=header).text
 soup = bs4.BeautifulSoup(response, 'lxml')
 
-coins = soup.find('div', {"id": "__APP"})
+divs = soup.find_all('div', {"direction": "ltr"})
+
+coins = {}
+
+for div in divs:
+    link_block = div.find_next('a', {"data-bn-type": "link"})
+    name_block = link_block.find_next('div', {"class": "body3 line-clamp-1 truncate text-t-third css-vurnku"})
+
+    link = link_block.get('href')
+    name = name_block.text
+
+    coins[name] = "https://www.binance.com" + link
+
+pprint(coins)
